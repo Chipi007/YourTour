a96db2a77d3ff59310a05c93b20eebfee3d49602
Chipi007 <94197615+Chipi007@users.noreply.github.com>
2022-01-21 23:19:19 +0300

    all is done

---
+++ JS/main_script.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+
+    $(function(){
+        $("#phone").mask("+9(999)999-99-99");
+    });
+
+    $(window).scroll(function() {     
+        if ($(window).scrollTop() > 450) {
+            $('.header').addClass('header__scrolled');
+            $('.menu__link a').css('color', '#1B1F2B');
+            jQuery('.menu__link').hover(
+                    function () {
+                        jQuery(this).addClass('hover');
+                    },
+                     function () {
+                    jQuery(this).removeClass('hover');
+                    });
+            $('.phone__number a').css('color', '#1B1F2B');
+            $('.logo img').css('filter', 'brightness(0) saturate(100%) invert(7%) sepia(4%) saturate(7476%) hue-rotate(188deg) brightness(98%) contrast(88%)');
+        } 
+        else {
+            $('.header').removeClass('header__scrolled');
+            jQuery('.menu__link').hover(
+                function () {
+                    jQuery(this).removeClass('hover');
+                });
+            $('.menu__link a').css('color', '#FFFFFF');
+            $('.phone__number a').css('color', '#FFFFFF');
+            $('.logo img').css('filter', 'invert(.0)');
+        }
+    });
+});
