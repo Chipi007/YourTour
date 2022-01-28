56bff009fa15504fcd932883b19fc4ab7622b4c2
Chipi007 <94197615+Chipi007@users.noreply.github.com>
2022-01-28 18:19:48 +0300

    all is done

--- JS/main_script.js
+++ JS/main_script.js
@@ -16,6 +16,13 @@ jQuery(document).ready(function() {
                     jQuery(this).removeClass('hover');
                     });
             $('.phone__number a').css('color', '#1B1F2B');
+            jQuery('.phone__number a').hover(
+                function () {
+                    jQuery(this).addClass('hover');
+                },
+                 function () {
+                jQuery(this).removeClass('hover');
+                });
             $('.logo img').css('filter', 'brightness(0) saturate(100%) invert(7%) sepia(4%) saturate(7476%) hue-rotate(188deg) brightness(98%) contrast(88%)');
         } 
         else {
@@ -26,6 +33,10 @@ jQuery(document).ready(function() {
                 });
             $('.menu__link a').css('color', '#FFFFFF');
             $('.phone__number a').css('color', '#FFFFFF');
+            jQuery('.phone__number a').hover(
+                function () {
+                    jQuery(this).removeClass('hover');
+                });
             $('.logo img').css('filter', 'invert(.0)');
         }
     });
