69132030c9069697fd8846f0ab2b6d318dc95297
Chipi007 <94197615+Chipi007@users.noreply.github.com>
2022-02-28 14:27:35 +0300

    fixed bugs

--- JS/main_script.js
+++ JS/main_script.js
@@ -1,43 +1,40 @@
 jQuery(document).ready(function() {
-
     $(function(){
-        $("#phone").mask("+9(999)999-99-99");
-    });
-
-    $(window).scroll(function() {     
-        if ($(window).scrollTop() > 450) {
-            $('.header').addClass('header__scrolled');
-            $('.menu__link a').css('color', '#1B1F2B');
-            jQuery('.menu__link').hover(
-                    function () {
-                        jQuery(this).addClass('hover');
-                    },
-                     function () {
-                    jQuery(this).removeClass('hover');
-                    });
-            $('.phone__number a').css('color', '#1B1F2B');
-            jQuery('.phone__number a').hover(
-                function () {
-                    jQuery(this).addClass('hover');
-                },
-                 function () {
-                jQuery(this).removeClass('hover');
-                });
-            $('.logo img').css('filter', 'brightness(0) saturate(100%) invert(7%) sepia(4%) saturate(7476%) hue-rotate(188deg) brightness(98%) contrast(88%)');
-        } 
-        else {
-            $('.header').removeClass('header__scrolled');
-            jQuery('.menu__link').hover(
-                function () {
-                    jQuery(this).removeClass('hover');
-                });
-            $('.menu__link a').css('color', '#FFFFFF');
-            $('.phone__number a').css('color', '#FFFFFF');
-            jQuery('.phone__number a').hover(
-                function () {
-                    jQuery(this).removeClass('hover');
-                });
-            $('.logo img').css('filter', 'invert(.0)');
-        }
+        $("#phone").mask("+7(999)999-99-99");
     });
 });
+
+document.addEventListener('scroll', () => {
+    changeHeader();
+});
+
+let changeHeader = () => {
+    let headerWrapper = document.querySelector('.header');
+    let headerLinks = document.querySelectorAll('.menu__link');
+    let logo = document.querySelector('.img');
+    let headerPhone = document.querySelector('.phone__number');
+    let screenHeight = innerHeight - 110;
+    let scrollTop = window.scrollY;
+    if(scrollTop >= screenHeight){
+        headerWrapper.classList.add('header__scrolled');
+        for(let i = 0; i < headerLinks.length; i++){
+            headerLinks[i].classList.add('black');
+            headerLinks[i].classList.remove('white');
+        }
+        logo.classList.add('filtered__logo');
+        headerPhone.classList.add('black');
+        headerPhone.classList.remove('white');
+    }
+    else{
+        headerWrapper.classList.remove('header__scrolled');
+        for(let i = 0; i < headerLinks.length; i++){
+            headerLinks[i].classList.remove('black');
+            headerLinks[i].classList.add('white');
+        }
+        logo.classList.remove('filtered__logo');
+        headerPhone.classList.add('white');
+        headerPhone.classList.remove('black');
+    }
+};
+
+changeHeader();
